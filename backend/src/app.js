0cd5fd1ede36a83ac5a007acd5957aae628c460b
Lina Cardona <Linajo2015@hotmail.com>
2025-12-04 17:33:44 -0500

    conecta la app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,11 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require('express')
+const app = express()
+const port = 3000
 
-// Cargar variables de entorno
-dotenv.config();
-
-const app = express();
-// Usa el puerto definido en tu archivo .env o 3000 por defecto
-const port = process.env.PORT || 3000;
-
-// Middleware para procesar solicitudes JSON (necesario para la API)
-app.use(express.json());
-
-// --- RUTA DE PRUEBA ---
 app.get('/', (req, res) => {
-  res.send('✅ Servidor de Sushi funcionando correctamente!');
-});
-// -----------------------
+  res.send('Funciona')
+})
 
-// Iniciar el servidor
 app.listen(port, () => {
-  console.log(`🚀 Servidor corriendo en http://localhost:${port}`);
-});
+  console.log(`Servidor corriendo ${port}`)
+})
